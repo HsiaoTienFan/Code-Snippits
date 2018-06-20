220e29c075d5190ca9cdd16c08fc2762aac1d7a9
Hsiao-Tien Fan <Hsiao-Tien.Fan@fonterra.com>
2018-06-21 09:49:08 +1200

    Removed action of splitting datetime format into individual columns from main app into own script.

---
+++ separateDateTime.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 09:44:45 2018
+
+@author: Fanhsiao
+"""
+
+
+
+time = Now[Now.columns[0]]
+time_formated = pd.DataFrame(pd.to_datetime(time, format = '%Y-%m-%dT%H:%M:%SZ'))
+time_formated['year'] = pd.DatetimeIndex(time_formated['sample_dt']).year
+time_formated['month'] = pd.DatetimeIndex(time_formated['sample_dt']).year
+time_formated['hour'] = pd.DatetimeIndex(time_formated['sample_dt']).year
+time_formated['minute'] = pd.DatetimeIndex(time_formated['sample_dt']).year
+time_formated['second'] = pd.DatetimeIndex(time_formated['sample_dt']).year
