b07157e8b5392db020aaa44d929d6d0ac0e9d09e
Hsiao-Tien Fan <Hsiao-Tien.Fan@fonterra.com>
2018-06-21 12:13:52 +1200

    separateDateTime assigns new column to 'Now' dataframe to indicate if plant 1 is running

--- separateDateTime.py
+++ separateDateTime.py
@@ -7,8 +7,9 @@ Created on Thu Jun 21 09:44:45 2018
 
 time = Now[Now.columns[0]]
 time_formated = pd.DataFrame(pd.to_datetime(time, format = '%Y-%m-%dT%H:%M:%SZ'))
-time_formated['year'] = pd.DatetimeIndex(time_formated['sample_dt']).year
-time_formated['month'] = pd.DatetimeIndex(time_formated['sample_dt']).year
-time_formated['hour'] = pd.DatetimeIndex(time_formated['sample_dt']).year
-time_formated['minute'] = pd.DatetimeIndex(time_formated['sample_dt']).year
-time_formated['second'] = pd.DatetimeIndex(time_formated['sample_dt']).year
+Now['year'] = pd.DatetimeIndex(time_formated['sample_dt']).year
+Now['month'] = pd.DatetimeIndex(time_formated['sample_dt']).month
+Now['hour'] = pd.DatetimeIndex(time_formated['sample_dt']).hour
+Now['minute'] = pd.DatetimeIndex(time_formated['sample_dt']).minute
+Now['second'] = pd.DatetimeIndex(time_formated['sample_dt']).second
+Now.assign(Mozz1_On = Now[labelNames['Curd Mill Output'][0]] >= 60)
